b978463e42cbe19d3415e4adb201fe935a89f501
ulug'bek <azizbekovulugbek8@gmail.com>
2025-03-07 11:01:38 +0500

    none

---
+++ index.js
@@ -0,0 +1,14 @@
+let hoursSpan = document. querySelector(".hours");
+let minutesSpan = document. querySelector(" minutes");
+let secondsSpan = document. querySelector(" seconds");
+
+function renderClock() {
+let hours = new Date().getHours();
+let minutes = new Date() •getMinutes();
+let seconds = new Date(). getSeconds();
+
+hoursSpan. textContent = hours;
+minutesSpan. textContent = minutes;
+secondsSpan. textContent = seconds;
+}
+setInterval( renderClock, 1000);

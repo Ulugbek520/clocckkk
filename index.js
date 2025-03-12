a1b6a54e9c075f9dd43a1f82d2cae8a90dd635d9
ulug'bek <azizbekovulugbek8@gmail.com>
2025-03-12 10:20:02 +0500

    gt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let hoursSpan = document. querySelector(".hours");
-let minutesSpan = document. querySelector(" minutes");
-let secondsSpan = document. querySelector(" seconds");
+let minutesSpan = document. querySelector(".minutes");
+let secondsSpan = document. querySelector(".seconds");
 
 function renderClock() {
 let hours = new Date().getHours();
-let minutes = new Date() •getMinutes();
+let minutes = new Date().  getMinutes();
 let seconds = new Date(). getSeconds();
 
 hoursSpan. textContent = hours;
